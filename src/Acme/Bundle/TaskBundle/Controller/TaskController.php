46b943fa96f923663e520707b5d3345223672c5c
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2014-01-18 15:22:03 +0200

    BAP-2818: Create bundle for training - section 5: Adding create/update/view actions and grid actions, using localization

--- src/Acme/Bundle/TaskBundle/Controller/TaskController.php
+++ src/Acme/Bundle/TaskBundle/Controller/TaskController.php
@@ -7,6 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Oro\Bundle\UserBundle\Entity\User;
+
+use Acme\Bundle\TaskBundle\Entity\Task;
+
 /**
  * @Route("/task")
  */
@@ -24,5 +28,81 @@ class TaskController extends Controller
     public function indexAction()
     {
         return array();
+    }
+
+    /**
+     * @Route("/create", name="acme_task_create")
+     * @Template("AcmeTaskBundle:Task:update.html.twig")
+     */
+    public function createAction()
+    {
+        $task = new Task();
+
+        $defaultStatus = $this->getDoctrine()->getManager()->find('AcmeTaskBundle:TaskStatus', 'open');
+        $task->setStatus($defaultStatus);
+
+        $securityToken = $this->get('security.context')->getToken();
+        if ($securityToken->getUser() instanceof User) {
+            $task->setOwner($securityToken->getUser());
+        }
+
+        return $this->update($task);
+    }
+
+    /**
+     * @Route("/view/{id}", name="acme_task_view", requirements={"id"="\d+"})
+     * @Template
+     */
+    public function viewAction(Task $task)
+    {
+        return array('entity' => $task);
+    }
+
+    /**
+     * @Route("/update/{id}", name="acme_task_update", requirements={"id"="\d+"})
+     * @Template
+     */
+    public function updateAction(Task $entity)
+    {
+        return $this->update($entity);
+    }
+
+    /**
+     * @param Task $task
+     * @return array
+     */
+    protected function update(Task $task)
+    {
+        $request = $this->getRequest();
+        $form = $this->createForm($this->get('acme_task.form.type.task'), $task);
+
+        if ('POST' == $request->getMethod()) {
+            $form->submit($request);
+            if ($form->isValid()) {
+                $this->getDoctrine()->getManager()->persist($task);
+                $this->getDoctrine()->getManager()->flush();
+
+                $this->get('session')->getFlashBag()->add(
+                    'success',
+                    $this->get('translator')->trans('acme.task.saved_message')
+                );
+
+                return $this->get('oro_ui.router')->actionRedirect(
+                    array(
+                        'route' => 'acme_task_update',
+                        'parameters' => array('id' => $task->getId()),
+                    ),
+                    array(
+                        'route' => 'acme_task_view',
+                        'parameters' => array('id' => $task->getId()),
+                    )
+                );
+            }
+        }
+
+        return array(
+            'entity' => $task,
+            'form' => $form->createView(),
+        );
     }
 }
